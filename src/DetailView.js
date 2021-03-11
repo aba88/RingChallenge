89f5d2d879e591ce41cdff63be09063b12bfc81e
aba88 <80277964+aba88@users.noreply.github.com>
2021-03-11 00:21:03 -0600

    Adding scroll support for posts and styles to detail view

--- src/DetailView.js
+++ src/DetailView.js
@@ -2,23 +2,28 @@ import React from 'react';
 
 
 const DetailView = ({title, createdOn, image, fullImage, author, comments,}) => {
-    
+    image = image.replace(/&amp;/g, '&');
 
     return(
 
 
-<div className={`post detailView`}>
+<div className={`detailView`}>
 
 
-        <div className="postSubcontainer">
-        <a href={fullImage} target="_blank" rel="noreferrer"><img src={image} alt=""/></a>
-            <div className="rightContainer">
+        <div className="postSubcontainer_detailView">
+        <div className="leftContainer_detailView">
+        <a href={image} target="_blank" rel="noreferrer"><img src={image} alt=""/></a>
+        </div>
+            
+            <div className="rightContainer_detailView">
             <h5>{title}</h5>
             <p><strong>Author: </strong> {author}</p>
             <p><strong>Created on:</strong> {createdOn}</p>
             <p><strong>Comments:</strong> {comments}</p>
-           
             </div>
+            
+           
+            
         </div>
 
     
